f1f04a14b74cf6a0ee4387ee49a0e01d0278eea9
Jeon Suji <quanxiuzhi3@gmail.com>
2022-10-24 22:46:54 +0900

    프로젝트 생성

---
+++ jsj/studyWell2/src/studyWell2/Solution2.java
@@ -0,0 +1,18 @@
+package studyWell2;
+
+/*
+ * 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/courses/30/lessons/12944?language=java
+ * 정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+ */
+public class Solution2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public double solution(int[] arr) {
+        double answer = 0;
+        return answer;
+    }
+}
