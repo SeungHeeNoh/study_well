f1403012728e0bbcb647cc764881427f29ee2c2f
testall0836 <testall0836@gmail.com>
2022-10-26 00:16:29 +0900

    두 번째 문제풀이

---
+++ kdk/NumberReverse.java
@@ -0,0 +1,26 @@
+package kdk;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
+ * 
+ * 문제 설명
+ * 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+ * 
+ * 
+ */
+public class NumberReverse {
+
+    public int[] solution(long input) {
+
+        String sInput = input + "";
+
+        return Stream.of(String.valueOf(sInput).split("")).mapToInt(Integer::parseInt).toArray();
+    }
+}
