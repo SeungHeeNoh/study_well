c8f14ec4038877c3e0a41d8d074cedc0cd7f4b0f
dong8090 <donggeun.kim0836@gmail.com>
2022-11-17 02:25:03 +0900

    짝수홀수 개수 및 짝수의합 문제풀이 완료

---
+++ kdk/짝수의합.java
@@ -0,0 +1,18 @@
+package kdk;
+
+import java.util.stream.IntStream;
+
+/**
+ * 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
+ * 
+ * 짝수 홀수 개수
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120831
+ 
+ */
+public class 짝수의합 {
+
+    public int solution(int n) {
+
+        return (int)IntStream.rangeClosed(0, n).filter(s -> s % 2 == 0).sum();
+    }
+}
