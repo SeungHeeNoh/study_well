a4b7f22644509ee1ed74c4a23a5c50171a22cd8b
testall0836 <testall0836@gmail.com>
2022-11-26 14:21:05 +0900

    불필요한 캐스팅 제거

--- kdk/배열의평균값.java
+++ kdk/배열의평균값.java
@@ -13,6 +13,6 @@ public class 배열의평균값 {
 
     public double solution(int[] numbers) {
 
-        return (double)Arrays.stream(numbers).average().orElse(0);
+        return Arrays.stream(numbers).average().orElse(0);
     }
 }
