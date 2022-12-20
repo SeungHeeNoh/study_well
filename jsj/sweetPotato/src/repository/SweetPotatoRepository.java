f46e5eccaeb89a82ff209ad4ab5c1d9eccbf4753
QuanXiuzhi <35889496+QuanXiuzhi@users.noreply.github.com>
2022-12-20 06:55:52 +0000

    hw: 10주차 과제 고구마 판매점 코드를 Predicate를 사용해 리팩토링 #52

---
+++ jsj/sweetPotato/src/repository/SweetPotatoRepository.java
@@ -0,0 +1,18 @@
+package repository;
+
+import java.util.List;
+
+import vo.SweetPotato;
+
+public interface SweetPotatoRepository {
+
+	List<SweetPotato> filterByType(String type);
+
+	List<SweetPotato> filterGreaterThan(int weight);
+
+	List<SweetPotato> filterByLessThan(int weight);
+
+	List<SweetPotato> filterGreaterThanAndType(int weight, String type);
+	
+	List<SweetPotato> filter(Predicate<SweetPotato> predicate);
+}
