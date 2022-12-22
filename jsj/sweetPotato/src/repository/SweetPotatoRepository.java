2e18837956c6d9d447b4c025326a1a15209acd33
QuanXiuzhi <35889496+QuanXiuzhi@users.noreply.github.com>
2022-12-22 00:48:28 +0000

    hw:10주차 과제 SweetPotatoRepository 사용하지 않는 메서드 제거 #52

--- jsj/sweetPotato/src/repository/SweetPotatoRepository.java
+++ jsj/sweetPotato/src/repository/SweetPotatoRepository.java
@@ -6,13 +6,6 @@ import vo.SweetPotato;
 
 public interface SweetPotatoRepository {
 
-	List<SweetPotato> filterByType(String type);
-
-	List<SweetPotato> filterGreaterThan(int weight);
-
-	List<SweetPotato> filterByLessThan(int weight);
-
-	List<SweetPotato> filterGreaterThanAndType(int weight, String type);
-	
 	List<SweetPotato> filter(Predicate<SweetPotato> predicate);
+	
 }
