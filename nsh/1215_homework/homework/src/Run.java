e9f6ba0da65e28768297a54a4e983356687df22a
nsh <nsh2398@naver.com>
2022-12-28 22:42:11 +0900

    hw : 1222 과제 세팅
    - 1222과제에 추가된 내용 적용
    
    #64

--- nsh/1215_homework/homework/src/Run.java
+++ nsh/1215_homework/homework/src/Run.java
@@ -16,5 +16,15 @@ public class Run {
 		System.out.println(sweetPotatoBox2.size() == 3);
 		System.out.println(sweetPotatoBox3.size() == 1);
 		System.out.println(sweetPotatoBox4.size() == 2);
+
+		List<Potato> potatoBox1 = sweetPotatoController./* type이 봄감자인 감자 */;
+		List<Potato> potatoBox2 = sweetPotatoController./* 무게가 330 이상인 감자 */;
+		List<Potato> potatoBox3 = sweetPotatoController./* 원산지가 제주인 감자 */;
+		List<Potato> potatoBox4 = sweetPotatoController./* 원산지가 봄감자가 아닌 감자 */;
+
+		System.out.println(potatoBox1.size() == 4);
+		System.out.println(potatoBox2.size() == 3);
+		System.out.println(potatoBox3.size() == 1);
+		System.out.println(potatoBox4.size() == 2);
 	}
 }
--- nsh/1215_homework/homework/src/repository/SweetPotatoRepositoryImpl.java
+++ nsh/1215_homework/homework/src/repository/SweetPotatoRepositoryImpl.java
@@ -9,6 +9,7 @@ import vo.SweetPotato;
 public class SweetPotatoRepositoryImpl implements SweetPotatoRepository {
 	
 	private List<SweetPotato> sweetPotatoField;
+	private List<Potato> potatoField;
 	
 	public SweetPotatoRepositoryImpl() {
 		sweetPotatoField = new ArrayList<>();
@@ -18,6 +19,14 @@ public class SweetPotatoRepositoryImpl implements SweetPotatoRepository {
 		sweetPotatoField.add(new SweetPotato("밤고구마", 333));
 		sweetPotatoField.add(new SweetPotato("호박고구마", 320));
 		sweetPotatoField.add(new SweetPotato("호박고구마", 610));
+
+		potatoField = new ArrayList<>();
+		potatoField.add(new Potato("봄감자", 200, "경기 여주"));
+		potatoField.add(new Potato("봄감자", 580, "전북 김제"));
+		potatoField.add(new Potato("봄감자", 150, "전남 보성"));
+		potatoField.add(new Potato("봄감자", 333, "경남 밀양"));
+		potatoField.add(new Potato("고랭지 감자", 320, "강원도 평창"));
+		potatoField.add(new Potato("가을 감자", 610, "제주"));
 	}
 
 	@Override
