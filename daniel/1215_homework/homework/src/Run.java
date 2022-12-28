ab7803e672da017179fcd395c857acc0e2c0d7c7
Danielyoon <danielyoon@dayside.co.kr>
2022-12-28 16:57:06 +0900

    hw. PR 리뷰 반영 #61

--- daniel/1215_homework/homework/src/Run.java
+++ daniel/1215_homework/homework/src/Run.java
@@ -5,12 +5,12 @@ import vo.SweetPotato;
 
 public class Run {
 	public static void main(String[] args) throws Exception {
-		SweetPotatoController bookController = new SweetPotatoController();
+		SweetPotatoController sweetPotatoController = new SweetPotatoController();
 
-		List<SweetPotato> sweetPotatoBox1 = bookController.filter( (a) -> a.getType().equals("밤고구마") );
-		List<SweetPotato> sweetPotatoBox2 = bookController.filter( (a) -> a.getWeight() > 500 );
-		List<SweetPotato> sweetPotatoBox3 = bookController.filter( (a) -> a.getWeight() < 200 );
-		List<SweetPotato> sweetPotatoBox4 = bookController.filter( (a) -> a.getWeight() > 500 && a.getType().equals("호박고구마") );
+		List<SweetPotato> sweetPotatoBox1 = sweetPotatoController.filter( (SweetPotato) -> "밤고구마".equals(SweetPotato.getType()) );
+		List<SweetPotato> sweetPotatoBox2 = sweetPotatoController.filter( (SweetPotato) -> SweetPotato.getWeight() > 500 );
+		List<SweetPotato> sweetPotatoBox3 = sweetPotatoController.filter( (SweetPotato) -> SweetPotato.getWeight() < 200 );
+		List<SweetPotato> sweetPotatoBox4 = sweetPotatoController.filter( (SweetPotato) -> SweetPotato.getWeight() > 500 && "호박고구마".equals(SweetPotato.getType()) );
 		
 		System.out.println(sweetPotatoBox1.size() == 3);
 		System.out.println(sweetPotatoBox2.size() == 3);
